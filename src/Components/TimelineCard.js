4b5ca1b3d818762ccb8ecf4a5a69c560bb1ebc9b
Luke Lapresi <lxl6996@rit.edu>
2018-10-16 08:07:22 -0400

    More stuff part 3

--- src/Components/TimelineCard.js
+++ src/Components/TimelineCard.js
@@ -15,41 +15,78 @@ const styles = {
     maxWidth: 600,
     marginBottom: '1rem',
     marginTop: '1rem',
+    transition: 'all 225ms cubic-bezier(0.4, 0.0, 0.2, 1);'
+  },
+  cardExpanded: {
+    width: '100%',
+    maxWidth: 850,
+    marginBottom: '2rem',
+    marginTop: '2rem',
+    transition: 'all 225ms cubic-bezier(0.4, 0.0, 0.2, 1);'
   },
   media: {
     height: 140,
+    transition: 'all 225ms cubic-bezier(0.4, 0.0, 0.2, 1);'
+  },
+  mediaExpanded: {
+    height: 600,
+    transition: 'all 225ms cubic-bezier(0.4, 0.0, 0.2, 1);'
   },
 };
 
-const TimelineCard = ({ name, image, description, date, classes }) => {
-  let cardMedia = '';
-  let dateString = '';
-  if (description.length > 170) {
-    description = description.substring(0, 167);
-    description += "...";
+class TimelineCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
   }
 
-  if (image !== undefined) {
-    cardMedia = (
-      <CardMedia
-        className={classes.media}
-        image={image}
-        title="Contemplative Reptile"
-      />
-    );
-  }
+  render() {
+    let { name, image, description, date, classes, source, imageDescription, imageSource } = this.props;
+    const { expanded } = this.state;
+        
+    let cardClassToUse = classes.card;
+    let mediaClassToUse = classes.media;
+    let buttonText = 'Learn More';
+    if (expanded) {
+      cardClassToUse = classes.cardExpanded;
+      mediaClassToUse = classes.mediaExpanded;
+      buttonText = 'Close';
+    }
 
-  if (date.day === undefined && date.month === undefined) {
-    dateString = `${date.year}`;
-  } else if (date.day === undefined) {
-    dateString = `${date.month}/${date.year}`;
-  } else {
-    dateString = `${date.month}/${date.day}/${date.year}`;
-  }
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        {cardMedia}
+
+    let cardMedia = '';
+    let dateString = '';
+    if (description.length > 170 && expanded === false) {
+      description = description.substring(0, 167);
+      description += "...";
+    }
+
+    if (image !== undefined) {
+      cardMedia = (
+        <CardMedia
+          className={mediaClassToUse}
+          image={image}
+          title="Contemplative Reptile"
+        />
+      );
+    }
+
+    if (date.day === undefined && date.month === undefined) {
+      dateString = `${date.year}`;
+    } else if (date.day === undefined) {
+      dateString = `${date.month}/${date.year}`;
+    } else {
+      dateString = `${date.month}/${date.day}/${date.year}`;
+    }
+
+
+    return (
+      <Card className={cardClassToUse}>
+        <CardActionArea onClick={() => this.setState({expanded: !expanded})}>
+          {cardMedia}
+        </CardActionArea>
         <CardContent>
           <Typography gutterBottom variant="headline" component="h2">
             {name}
@@ -57,18 +94,45 @@ const TimelineCard = ({ name, image, description, date, classes }) => {
           <Typography variant="overline" gutterBottom>
             {dateString}
           </Typography>
-          <Typography component="p">
+          <Typography variant="body1" gutterBottom>
             {description}
           </Typography>
+          {source !== undefined && expanded
+            && (
+              <Typography variant="caption" gutterBottom>
+                Source: {source}
+              </Typography>
+            )
+          }
+          {imageDescription !== undefined && expanded
+            && (
+              <React.Fragment>
+                <Typography variant="h6" gutterBottom>
+                  Image:
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {imageDescription}
+                </Typography>
+              </React.Fragment>
+            )
+          }
+          {imageSource !== undefined && expanded
+            && (
+              <Typography variant="caption" gutterBottom>
+                Image Source: {imageSource}
+              </Typography>
+            )
+          }
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+        <CardActions>
+          <Button size="small" onClick={() => this.setState({expanded: !expanded})}>
+            {buttonText}
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
+  
 }
 
 TimelineCard.propTypes = {
