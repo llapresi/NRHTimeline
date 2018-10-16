7078e2e6fa1121c6b43ed8b435d90aed4969f4a5
Luke Lapresi <lxl6996@rit.edu>
2018-10-16 08:20:46 -0400

    ugh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TimelineCard from './Components/TimelineCard';
 import TimelineData from './Data';
